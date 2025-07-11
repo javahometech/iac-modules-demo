99f7c8ddc9a7908073e0a250e607d58303b7a629
javahometech <hari.kammana@gmail.com>
2025-07-11 16:59:23 +0530

    Update main.tf

--- s3/modules/s3-bucket-policy/main.tf
+++ s3/modules/s3-bucket-policy/main.tf
@@ -2,16 +2,5 @@
 resource "aws_s3_bucket_policy" "public_read_policy" {
   bucket = var.s3_bucket_id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${var.s3_bucket_arn}/*"
-      }
-    ]
-  })
+  policy = var.s3_bucket_policy
 }
