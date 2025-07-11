e068d0aee4fdbe9f0b564a27e42c02ba3e03b50b
javahometech <hari.kammana@gmail.com>
2025-07-11 16:21:47 +0530

    Update main.tf

--- s3/modules/s3-bucket/main.tf
+++ s3/modules/s3-bucket/main.tf
@@ -2,3 +2,8 @@ resource "aws_s3_bucket" "main" {
   bucket = var.bucket_name
   tags = var.tags
 }
+
+resource "aws_s3_bucket_policy" "main" {
+  bucket = aws_s3_bucket.this.id
+  policy = var.policy_json
+}
